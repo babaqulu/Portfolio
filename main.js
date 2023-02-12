29560aa733a37e9126235f333cf1381741cd8fec
babaqulu <baba.qulu97@gmail.com>
2023-02-13 02:52:49 +0400

    Changed Js

---
+++ main.js
@@ -0,0 +1,10 @@
+let x = document.getElementById('hb')
+
+function data5 (){
+    
+document.getElementById('hb').style = 'visibility:hidden;'
+document.getElementById('ex').style = 'visibility: visible;'
+  
+}
+copnsole.log(x)
+data5()
