e5bcccce11ba58d67c05b15c05af328ef42dc4fa
babaqulu <baba.qulu97@gmail.com>
2023-02-13 23:33:16 +0400

    JS

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let x = document.getElementById('hb')
+let y = document.getElementById('hmenu').style = 'transform: translateY(-800px);'
 
 function data5 (){
     
 document.getElementById('hb').style = 'visibility:hidden;'
 document.getElementById('ex').style = 'visibility: visible;'
-  
+document.getElementById('hmenu').style = 'transform: translateY(0px);'
 }
-copnsole.log(x)
-data5()
+
+function data6 (){
+    
+  document.getElementById('hb').style = 'visibility:visible;'
+  document.getElementById('ex').style = 'visibility: hidden;'
+  document.getElementById('hmenu').style = 'visibility:hidden;'
+
+  }
